6aea6b292a18f963ba224a730565254b6d975f18
Rohan Kumar Dwivedi <rohandwivedi1203@gmail.com>
2024-03-04 16:01:22 +0530

    Logic Added

---
+++ script.js
@@ -0,0 +1,35 @@
+let boxes = document.querySelectorAll(".btn");
+let resetBtn = document.querySelector("#reset");
+
+let turn0 = true;
+
+let pattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+let checkWinner = () => {
+    for (let pat of pattern){
+        
+    }
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turn0) {
+      box.innerText = "O";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
